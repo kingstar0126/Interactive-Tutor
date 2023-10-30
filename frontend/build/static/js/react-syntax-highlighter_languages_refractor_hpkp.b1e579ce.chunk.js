789a1fe2d9f7d6e24b233b6224ff7d85259f2a8a
Ubuntu <ubuntu@ip-172-31-3-237.eu-west-2.compute.internal>
2023-10-30 07:44:48 +0000

    build

--- frontend/build/static/js/react-syntax-highlighter_languages_refractor_hpkp.b1e579ce.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkAI_Tutor=self.webpackChunkAI_Tutor||[]).push([[6749],{15712:function(a){function e(a){a.languages.hpkp={directive:{pattern:/\b(?:(?:includeSubDomains|preload|strict)(?: |;)|pin-sha256="[a-zA-Z\d+=/]+"|(?:max-age|report-uri)=|report-to )/,alias:"keyword"},safe:{pattern:/\d{7,}/,alias:"selector"},unsafe:{pattern:/\d{1,6}/,alias:"function"}}}a.exports=e,e.displayName="hpkp",e.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hpkp.b1e579ce.chunk.js.map
--- frontend/build/static/js/react-syntax-highlighter_languages_refractor_properties.3038380d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkAI_Tutor=self.webpackChunkAI_Tutor||[]).push([[81],{91313:function(t){function e(t){t.languages.properties={comment:/^[ \t]*[#!].*$/m,"attr-value":{pattern:/(^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?: *[=:] *| ))(?:\\(?:\r\n|[\s\S])|[^\\\r\n])+/m,lookbehind:!0},"attr-name":/^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?= *[=:] *| )/m,punctuation:/[=:]/}}t.exports=e,e.displayName="properties",e.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_properties.3038380d.chunk.js.map
