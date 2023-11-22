8b13ba71608d5cc6c918447cf3193dc2311e5a6b
kingstar0126 <popstar0982@gmail.com>
2023-11-22 18:17:25 -0500

    update the interactive-tutor.service.sh

--- backend/interactive-tutor.service.sh
+++ backend/interactive-tutor.service.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git pull
 cd /home/ubuntu/Interactive-Tutor/backend
-source /home/ubuntu/Interactive-Tutor/backend/.venv/bin/activate
+source /home/ubuntu/Interactive-Tutor/backend/venv/bin/activate
+pip install -r requirements.txt
 python3 wsgi.py
