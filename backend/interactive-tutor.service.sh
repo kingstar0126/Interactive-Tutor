f5638bbac3244a16c5a75df2313ca4f1e56dab6e
Ubuntu <ubuntu@ip-172-31-26-96.eu-west-2.compute.internal>
2023-11-23 09:24:26 +0000

    update the sh file

--- backend/interactive-tutor.service.sh
+++ backend/interactive-tutor.service.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 git pull
+cd /home/ubuntu/Interactive-Tutor/frontend
+yarn install
+yarn build
 cd /home/ubuntu/Interactive-Tutor/backend
 source /home/ubuntu/Interactive-Tutor/backend/venv/bin/activate
 pip install -r requirements.txt
