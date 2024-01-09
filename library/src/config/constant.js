1d1d921c52d19bdf5bb73f72507c15de8083e801
kingstar0126 <popstar0982@gmail.com>
2024-01-09 05:08:22 +0000

    Constant file

--- library/src/config/constant.js
+++ library/src/config/constant.js
@@ -1,4 +1,4 @@
-// export const SERVER_URL = "http://18.133.183.77"
+// export const SERVER_URL = "https://app.interactive-tutor.com"
 // export const EMBED_SERVER_URL = "https://embed.interactive-tutor.com"
 
 export const SERVER_URL = "http://18.133.183.77"
